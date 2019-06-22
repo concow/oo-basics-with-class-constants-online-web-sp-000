a0a04c2e0d0078a55d13a9133b936f4934449fd6
Connor Cowan <ccowan003@comcast.net>
2019-06-22 01:48:33 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,7 +6,7 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    BRANDS << brand unless BRANDS.include?(brand)
+    BRANDS << brand unless BRANDS.include?(brand)         #add BRANDS to array unless its already included
   end
 
   def cobble
